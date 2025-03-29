30585dea8678e7d8a965a812e275bf7c43f63b7c
Supratim <109270340+sgindeed@users.noreply.github.com>
2025-03-29 10:12:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from tensorflow.keras.applications import VGG16
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 import joblib
+import os
 
 app = FastAPI()
 
